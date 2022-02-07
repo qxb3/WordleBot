52c14e87765f33c6ce978e82faa1b3fe3447fa85
qxb3 <toolong972@gmail.com>
2022-02-07 23:39:41 +0800

    Added mongodb

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const { SapphireClient } = require('@sapphire/framework')
 
+const mongoose = require('mongoose')
+
 require('module-alias/register')
 require('dotenv').config()
 
@@ -7,8 +9,21 @@ const client = new SapphireClient({
   intents: [
     'GUILDS',
     'GUILD_MESSAGES'
-  ],
-  defaultPrefix: '!'
+  ]
 })
+
+const connectMongoose = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      keepAlive: true
+    })
+    client.logger.info('Mongodb is successfuly connected.')
+  } catch(err) {
+    client.logger.error(err)
+  }
+}
+
+connectMongoose()
+
 
 client.login(process.env.TOKEN)
